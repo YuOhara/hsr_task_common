dc5c53500d2758203163299816e8f9ec567524c1
YuOhara <ohara@jsk.imi.i.u-tokyo.ac.jp>
2015-09-01 10:46:04 +0900

    change ui

--- scripts/ui_publisher.py
+++ scripts/ui_publisher.py
@@ -5,6 +5,7 @@ from sensor_msgs.msg import Image
 from hsr_task_common.msg import UiOut
 from dynamic_reconfigure.server import Server
 from hsr_task_common.cfg import AdditionalRqtConfig
+from std_srvs.srv import *
 
 class UiOutPublisher():
     def __init__(self):
@@ -13,15 +14,43 @@ class UiOutPublisher():
         rate = rospy.get_param('~rate', 10)
         r = rospy.Rate(rate)
         self.ui_out_pub = rospy.Publisher("/ui_out", UiOut)
+        self.task = 0
+        self.hours = 0
+        self.minutes = 0
         self.call_msg = ""
         self.message = ""
         self.photo_msg = None
+        rospy.Service('/send', Empty, self.send_cb)
+        rospy.Subscriber('/image_raw', Image, self.image_cb)
+        rospy.Service('/message', Empty, self.send_cb)
+        rospy.Service('/eat', Empty, self.eat_cb)
+        rospy.Service('/study', Empty, self.study_cb)
+        rospy.Service('/sleep', Empty, self.sleep_cb)   
     def reconfigure_callback(self, config, level):
         self.call_msg = config.call_msg
         self.message = config.message
+        self.hours = config.hours
+        self.minutes = config.minutes
         rospy.loginfo(self.call_msg)
         return config
-
+    def image_cb(self, msg):
+        self.photo_msg = msg
+    def send_cb(self, req):
+        self.ui_out_pub.publish(UiOut(face=self.photo_msg, call_msg=self.call_msg, message=self.message, obj_id=self.task, hours=self.hours, minutes=self.minutes))
+        return EmptyResponse()
+    def message_cb(self, req):
+        self.task = 0
+        return EmptyResponse()
+    def eat_cb(self, req):
+        self.task = 1
+        return EmptyResponse()
+    def study_cb(self, req):
+        self.task = 2
+        return EmptyResponse()
+    def sleep_cb(self, req):
+        self.task = 3
+        return EmptyResponse()
+    
 
 
 

bd406c6a5c15486047f4ea210fafc24fd6db65de
YuOhara <ohara@jsk.imi.i.u-tokyo.ac.jp>
2015-09-02 18:58:11 +0900

    hoge

--- scripts/get_sound.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env sh
-
-sshpass -p "hsruser" ssh hsr-user@hsrb "arecord -d 6 out.wav"
-sshpass -p "hsruser" scp hsr-user@hsrb:/home/hsr-user/out.wav `rospack find hsr_task_common`/out.wav
